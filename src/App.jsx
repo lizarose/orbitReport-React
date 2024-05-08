50051f5dbbc4d266df6489c238cba09bd31e0a0a
Elizabeth Levinson <lizarose266@gmail.com>
2024-05-08 07:07:04 -0500

    Completed Assignment 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import Banner from "./components/Banner";
+import { useState } from "react";
+import satData from "./components/satData";
 
 
 function App() {
+  //compare changes in state and update the state
+  const [sat, setSat] = useState(satData);
+
+  //uses Set method to create new array from satData- will be used to create buttons
+  const displaySats = [...new Set(satData.map((data) =>
+  data.orbitType))];
+
+  //filters satellite data based on orbit type selected, making sure only the orbit types shown match the selected button
+  const filterByType = (currentType) => {
+    const displaySats = satData.filter((newSatDisplay) => {
+       return newSatDisplay.orbitType === currentType;
+    });
+    setSat(displaySats);
+ };
+
   return (
     <div>
-      <Buttons />
-      <Table />
+      <Banner />
+      <Buttons 
+        filterByType={filterByType} 
+        setSat={setSat}             
+        displaySats={displaySats}   
+      />
+      <Table sat={sat}/>
     </div>
   );
 }
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,19 @@
+import satData from "./satData";
+import style from './styling.css'
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
+  
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-    </div>
-  );
-};
+    <div className="flex-container">
+      {/* map over displaySats to create button for each orbit type */}
+      {displaySats.map((sat, id) => (
+        <div>
+        <button onClick={() => filterByType(sat)} key={id}>{sat} Orbit</button>
+        </div>
+      ))}
+      <button onClick={() => setSat(satData)}>All Orbits</button>
+      </div>
+  )
+}
 
 export default Buttons;
